60290d49bcf07a75f8ee21f5a0fade5d146637b9
eldadfux <eldad.fux@gmail.com>
2016-08-05 15:14:16 +0300

    Added videos size constants

--- src/FFMPEG.php
+++ src/FFMPEG.php
@@ -814,88 +814,40 @@ class FFMPEG
     const VIDEO_SIZE_SVGA       = 'svga'; // 800x600
     const VIDEO_SIZE_XGA        = 'xga'; // 1024x768
     const VIDEO_SIZE_UXGA       = 'uxga'; // 1600x1200
-    const VIDEO_SIZE_qxga       = 'qxga'; // 2048x1536
-    const VIDEO_SIZE_sxga       = 'sxga'; // 1280x1024
-    const VIDEO_SIZE_qsxga      = 'qsxga'; // 2560x2048
-    const VIDEO_SIZE_hsxga      = 'hsxga'; // 5120x4096
-    const VIDEO_SIZE_wvga       = 'wvga'; // 852x480
-    const VIDEO_SIZE_wxga       = 'wxga'; // 1366x768
-    const VIDEO_SIZE_wsxga      = 'wsxga'; // 1600x1024
-    const VIDEO_SIZE_wuxga      = 'wuxga'; // 1920x1200
-    const VIDEO_SIZE_woxga      = 'woxga'; // 2560x1600
-    const VIDEO_SIZE_wqsxga     = 'wqsxga'; // 3200x2048
-    const VIDEO_SIZE_wquxga     = 'wquxga'; // 3840x2400
-    const VIDEO_SIZE_whsxga     = 'whsxga'; // 6400x4096
-    const VIDEO_SIZE_whuxga     = 'whuxga'; // 7680x4800
-    const VIDEO_SIZE_cga       = 'cga'; // 320x200
-    const VIDEO_SIZE_       = 'xxx'; //
-    const VIDEO_SIZE_       = 'xxx'; //
-    const VIDEO_SIZE_       = 'xxx'; //
-    const VIDEO_SIZE_       = 'xxx'; //
-
-
-‘cga’
-
-
-‘ega’
-640x350
-
-‘hd480’
-852x480
-
-‘hd720’
-1280x720
-
-‘hd1080’
-1920x1080
-
-‘2k’
-2048x1080
-
-‘2kflat’
-1998x1080
-
-‘2kscope’
-2048x858
-
-‘4k’
-4096x2160
-
-‘4kflat’
-3996x2160
-
-‘4kscope’
-4096x1716
-
-‘nhd’
-640x360
-
-‘hqvga’
-240x160
-
-‘wqvga’
-400x240
-
-‘fwqvga’
-432x240
-
-‘hvga’
-480x320
-
-‘qhd’
-960x540
-
-‘2kdci’
-2048x1080
-
-‘4kdci’
-4096x2160
-
-‘uhd2160’
-3840x2160
-
-‘uhd4320’
-7680x4320
+    const VIDEO_SIZE_QXGA       = 'qxga'; // 2048x1536
+    const VIDEO_SIZE_SXGA       = 'sxga'; // 1280x1024
+    const VIDEO_SIZE_QSXGA      = 'qsxga'; // 2560x2048
+    const VIDEO_SIZE_HSXGA      = 'hsxga'; // 5120x4096
+    const VIDEO_SIZE_WVGA       = 'wvga'; // 852x480
+    const VIDEO_SIZE_WXGA       = 'wxga'; // 1366x768
+    const VIDEO_SIZE_WSXGA      = 'wsxga'; // 1600x1024
+    const VIDEO_SIZE_WUXGA      = 'wuxga'; // 1920x1200
+    const VIDEO_SIZE_WOXGA      = 'woxga'; // 2560x1600
+    const VIDEO_SIZE_WQSXGA     = 'wqsxga'; // 3200x2048
+    const VIDEO_SIZE_WQUXGA     = 'wquxga'; // 3840x2400
+    const VIDEO_SIZE_WHSXGA     = 'whsxga'; // 6400x4096
+    const VIDEO_SIZE_WHUXGA     = 'whuxga'; // 7680x4800
+    const VIDEO_SIZE_CGA        = 'cga'; // 320x200
+    const VIDEO_SIZE_EGA        = 'ega'; // 640x350
+    const VIDEO_SIZE_HD480      = 'hd480'; // 852x480
+    const VIDEO_SIZE_HD720      = 'hd720'; // 1280x720
+    const VIDEO_SIZE_HD1080     = '1920x1080'; //
+    const VIDEO_SIZE_2K         = '2k'; // 2048x1080
+    const VIDEO_SIZE_2KFLAT     = '2kflat'; // 1998x1080
+    const VIDEO_SIZE_2KSCOPE    = '2kscope'; // 2048x858
+    const VIDEO_SIZE_4K         = '4k'; // 4096x2160
+    const VIDEO_SIZE_4KFLAT     = '4kflat'; // 3996x2160
+    const VIDEO_SIZE_4KSCOPE    = '4kscope'; // 4096x1716
+    const VIDEO_SIZE_NHD        = 'nhd'; // 640x360
+    const VIDEO_SIZE_HQVGA      = 'hqvga'; // 240x160
+    const VIDEO_SIZE_WQVGA      = 'wqvga'; // 400x240
+    const VIDEO_SIZE_FWQVGA     = 'fwqvga'; // 432x240
+    const VIDEO_SIZE_HVGA       = 'hvga'; // 480x320
+    const VIDEO_SIZE_QHD        = 'qhd'; // 960x540
+    const VIDEO_SIZE_2KDCI      = '2kdci'; // 2048x1080
+    const VIDEO_SIZE_4KDCI      = '4kdci'; // 4096x2160
+    const VIDEO_SIZE_UHD2160    = 'uhd2160'; // 3840x2160
+    const VIDEO_SIZE_UHD4320    = 'uhd4320'; // 7680x4320
 
     protected $bin;
 
